8c42fb4bf8c51ce9a28d14d580a37b2e76308107
Mukesh Kushwaha <singhmukesh41866@gmail.com>
2026-09-20 14:04:50 +0530

    add  backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,5 +4,5 @@
 
 
 
-export const BASE_URL = 
- location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+export const BASE_URL =
+    location.hostname === "localhost" ? "http://localhost:7777" : "https://devtinder-6vqk.onrender.com";
